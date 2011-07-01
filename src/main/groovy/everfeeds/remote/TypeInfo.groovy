5c83a9d35862f06edf49794c9cd91ece91956381
Dmitry Kurinskiy <name.alari@gmail.com>
2011-07-02 02:18:47 +0400

    AccessTypeInfo builder

--- src/main/groovy/everfeeds/remote/TypeInfo.groovy
+++ src/main/groovy/everfeeds/remote/TypeInfo.groovy
@@ -1,10 +1,16 @@
-package everfeeds.remote
+@Typed package everfeeds.remote
 
 import everfeeds.thrift.ttype.AccessTypeInfo
 import everfeeds.thrift.ttype.AccessType
 import everfeeds.util.Package
 import everfeeds.thrift.util.Type
 import everfeeds.util.annotation.Accessor
+import everfeeds.util.annotation.NotImplemented
+import everfeeds.util.annotation.NotSupported
+import everfeeds.mongo.CategoryD
+import everfeeds.mongo.TagD
+import everfeeds.mongo.EntryD
+import everfeeds.util.annotation.NoUpdatesSupported
 
 /**
  * @author Dmitry Kurinskiy
@@ -24,11 +30,24 @@ class TypeInfo {
   }
 
   static private void buildInfo(Type type, Class<Remote> cls) {
-    AccessTypeInfo info = new AccessTypeInfo()[type:type.toThrift(), isRemote: true]
+    AccessTypeInfo info = new AccessTypeInfo()[accessType:type.toThrift(), isRemote: true]
+
+    info.withEntries = true
+    info.withFiles = false
+
+    info.pullEntries = !cls.getMethod("pull").getAnnotation(NotImplemented) && !cls.getMethod("pull").getAnnotation(NotSupported)
+    info.pullFiles = false
+
+    info.pushCategory = !cls.getMethod("push", CategoryD).getAnnotation(NotImplemented) && !cls.getMethod("push", CategoryD).getAnnotation(NotSupported)
+    info.pushTag = !cls.getMethod("push", TagD).getAnnotation(NotImplemented) && !cls.getMethod("push", TagD).getAnnotation(NotSupported)
+    info.pushEntry = !cls.getMethod("push", EntryD).getAnnotation(NotImplemented) && !cls.getMethod("push", EntryD).getAnnotation(NotSupported)
+
+    info.updateEntry = !cls.getMethod("push", EntryD).getAnnotation(NoUpdatesSupported)
+
     infoMap.put(type.toThrift(), info)
   }
 
   static public AccessTypeInfo getInfo(AccessType type) {
-    infoMap.containsKey(type) ? infoMap.get(type) : new AccessTypeInfo()[type: type, isRemote: false]
+    infoMap.containsKey(type) ? infoMap.get(type) : (new AccessTypeInfo()[accessType: type, isRemote: false])
   }
 }
